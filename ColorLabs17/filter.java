72cef7df054dfc786d686d297b6f89b49b1d9a96
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2022-05-13 15:09:32 -0500

    recursive

--- ColorLabs17/filter.java
+++ ColorLabs17/filter.java
@@ -13,9 +13,9 @@ public class filter
     public static void main  (String args [])
     {
         Picture pic = new Picture("images\\og.jpg");
-        Picture apic = recursive(pic, 5);
-        //mirrorHorizontal(pic);
-        apic.explore();
+        //Picture apic = recursive(pic, 5);
+        contrast(pic);
+        pic.explore();
     }
     
     public static void addBlue(Picture pic)
@@ -101,11 +101,11 @@ public class filter
     
     public static Picture recursive(Picture pic, int x)
     {
-        if (x == 0)
-        {
+         if (x == 0)
+         {
             return pic;
-        }
-        Picture copy = new Picture (pic);
+         }
+         Picture copy = new Picture (pic);
           int width; int height;
           Pixel spot; Pixel spot1;
           int h = pic.getHeight() - 1;
@@ -127,8 +127,46 @@ public class filter
                   r1--;
                 }
             }
-          return copy;
+            
+          return recursive(copy, x-1);
+    }
+    
+    public static void contrast(Picture pic)
+    {
+        Pixel[] markspixs = pic.getPixels();
+       
+       for (Pixel apix: markspixs)
+       {
+           int r = apix.getRed();
+           int g = apix.getGreen();
+           int b = apix.getBlue();
+           int avg = (r + b + g)/3;
+           if (avg < 125)
+           {
+               if (r < 10 || g < 10 || b < 10)
+               {
+                   apix.setColor(new Color(0, 0, 0));
+                }
+               else
+               {
+                   apix.setColor(new Color(r - 10, g-10, b -10));
+                }
+            }
+           else
+           {
+               if (r > 240 || g > 240 || b > 240)
+               {
+                   apix.setColor(new Color(250, 250, 250));
+                }
+               else
+               {
+                apix.setColor(new Color(r + 10, g + 10, b + 10));
+               }
+            }
+        }
+        
     }
 }
+
 
     
